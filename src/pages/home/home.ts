93989dfafd9c1bdad74aa3bb8dce8b723d82037e
unknown <emanuelgobiluzia@gmail.com>
2019-05-26 00:40:49 -0300

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { IntroPage } from '../intro/intro';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+
+
+
+  gotoIntroPage(){
+    this.navCtrl.push(IntroPage) 
+    
+  }
+
+}
